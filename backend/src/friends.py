f9a038cd6856069d54373438e77804d2cc4af782
rndmemex <rndmemex@protonmail.com>
2021-04-04 00:36:40 +0100

    add new dd_friends

--- backend/src/friends.py
+++ backend/src/friends.py
@@ -46,8 +46,32 @@ def fetch_friends(user_id, db_uri):
 
     return friends_list
 
+def contains(arr, element):
+    if element in arr: 
+        return 1 #true
+    else:
+        return 0 #false
 
 
-def insert_friend(friend_id, db_uri):
- 
-    # insert a friend to the list of friends
+# When the user with user_id clicks on `add` or `connect` the list of friends is updated 
+# and a new friend with id = friend_id is added to the list
+
+def add_dd_friend(user_id, friend_id, db_uri):
+    client = pymongo.MongoClient(db_uri)
+    db = client["Main"]
+    coll = db["User"]
+    query = {"_id": user_id}
+    # I use the method $addToSet rather than $push, if the id already exist it will not be added
+    newvalues = {"$addToSet":{ "friends": friend_id}}
+    # update user record with _id =user_id with a new friend by appending
+    # an dd_id = friend_id to the friends array
+    coll.update_one(query, newvalues)
+    #confirmation 
+    for records in coll.find(query):
+        for record in records:
+            friends = record["friends"]
+            if contains(friends, friend_id) == 0:
+                print("FRIEND ADDED!")
+            else: 
+             print("FRIEND ALREADY ADDED")
+
---
+++ server.py
@@ -0,0 +1,65 @@
+import json
+import io
+import hashlib 
+from flask import Flask, request, render_template, url_for, make_response, jsonify, send_file
+from flask_mongoengine import MongoEngine
+from constants import *
+import requests
+app = Flask(__name__)
+mongodb_pass = '4v38Z9oFAxnKm6SG'
+db_name = "Main"
+# alternative
+# mongodb_pass = os.environ["MONGO_PWD"]
+# db_name = os.environ["MONGO_DB"]
+# DB_URI = os.environ["MONGODB_URI"]
+DB_URI = "mongodb+srv://s3kim2018:{}@cluster0.xfm8y.mongodb.net/{}?retryWrites=true&w=majority".format(mongodb_pass, db_name)
+app.config["MONGODB_HOST"] = DB_URI
+db = MongoEngine()
+db.init_app(app)
+
+app = Flask(__name__)
+
+class User(db.Document):
+    username = db.StringField()
+    password = db.StringField()
+    email = db.StringField() #adding email address field to the document
+    investmentstyle = db.StringField() #Choice of Value, Growth, Speculative
+    investmenthorizon = db.StringField() #Choice of Day, Monthly, Annual, Long-Term
+
+    def to_json(self): 
+        return {
+            "username": self.username,
+            "email":self.email, #adding email address field to JSON
+            "investmentstyle": self.investmentstyle, 
+            "investmenthorizon": self.investmenthorizon,
+
+        }
+
+
+@app.route("/")
+def mainpage():
+    response = requests.get('https://sandbox.tradier.com/v1/markets/search',
+        params={'q': 'alphabet', 'indexes': 'false'},
+        headers={'Authorization': 'Bearer EQGhrbEgpNGAVsdNEDJ4RT0TJoBY', 'Accept': 'application/json'}
+    )
+    print("hello")
+    #json_response = response.json()
+    print(response.status_code)
+    #print(json_response)
+    return "bob"
+
+
+#Example http://127.0.0.1:5000/register?username=bob&style=long&horizon=annual       
+
+@app.route("/register", methods = ['POST'] )
+def register(): 
+    username = request.args.get("username")
+    #password = request.form['password']
+    style = request.args.get("style")
+    horizon = request.args.get("horizon")
+    print(username)
+    return make_response("success", 201)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
