ed4609c37c1556d2faacb9e2a71f9865cb0d8ba5
Brian Kim <s3kim2018@berkeley.edu>
2021-04-03 04:34:09 -0700

    db set up

--- server.py
+++ server.py
@@ -5,8 +5,30 @@ from flask import Flask, request, render_template, url_for, make_response, jsoni
 from flask_mongoengine import MongoEngine
 from constants import *
 import requests
+app = Flask(__name__)
+mongodb_pass = '4v38Z9oFAxnKm6SG'
+db_name = "Main"
+DB_URI = "mongodb+srv://s3kim2018:{}@cluster0.xfm8y.mongodb.net/{}?retryWrites=true&w=majority".format(mongodb_pass, db_name)
+app.config["MONGODB_HOST"] = DB_URI
+db = MongoEngine()
+db.init_app(app)
 
 app = Flask(__name__)
+
+class User(db.Document):
+    username = db.StringField()
+    password = db.StringField()
+    investmentstyle = db.StringField() #Choice of Value, Growth, Speculative
+    investmenthorizon = db.StringField() #Choice of Day, Monthly, Annual, Long-Term
+
+    def to_json(self): 
+        return {
+            "username": self.username,
+            "investmentstyle": self.investmentstyle, 
+            "investmenthorizon": self.investmenthorizon,
+
+        }
+
 
 @app.route("/")
 def mainpage():
@@ -19,6 +41,19 @@ def mainpage():
     print(response.status_code)
     #print(json_response)
     return "bob"
+
+
+#Example http://127.0.0.1:5000/register?username=bob&style=long&horizon=annual       
+
+@app.route("/register", methods = ['POST'] )
+def register(): 
+    username = request.args.get("username")
+    #password = request.form['password']
+    style = request.args.get("style")
+    horizon = request.args.get("horizon")
+    print(username)
+    return make_response("success", 201)
+
 
 if __name__ == '__main__':
     app.run(debug = True)
